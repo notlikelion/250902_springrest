c9a32e19d126cdf6860d888351a95bbd42af9aea
qus0in <qus0in@gmail.com>
2025-09-02 11:26:28 +0900

    ㅎㅎ

--- src/main/java/com/example/springrest/controller/UserApiController.java
+++ src/main/java/com/example/springrest/controller/UserApiController.java
@@ -3,14 +3,17 @@ package com.example.springrest.controller;
 import com.example.springrest.dto.UserDto;
 import com.example.springrest.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+//@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/users") // prefix, 공통경로
 // localhost:8080/api/users/...
@@ -22,9 +25,17 @@ public class UserApiController {
     // public String index(Model model) { return "index"; }
     // public String index(RedirectAttributes...) { return "redirect..."; }
     // Session? Cookie? -> Response
-    @ResponseBody // ViewResolver로 가는게 아니라 Return 데이터 자체를 응답값으로 제공해라
+//    @ResponseBody // ViewResolver로 가는게 아니라 Return 데이터 자체를 응답값으로 제공해라
+    // -> 이건 @RestController에는 필요 없음 -> 혼용할 때는 필요할 수도...
     @GetMapping // GET localhost:8080/api/users
-    public List<UserDto.Response> getAllUsers() {
-        return userService.findAllUsers();
+//    public List<UserDto.Response> getAllUsers() {
+    public ResponseEntity<List<UserDto.Response>> getAllUsers() {
+//        return userService.findAllUsers();
+        // 응답종류를 처리하는 ResponseEntity로 감싸서 전달
+
+        // DB 원래 데이터 -> Entity (JPA Repo) -> Service, Controller (DTO)
+        // -> Restful API (JSON) / Thymeleaf (Model, Object)
+
+        return ResponseEntity.ok(userService.findAllUsers());
     }
 }
